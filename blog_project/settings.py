c41c610751512049b036113e2a328bcc7d597dd3
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-05 09:11:54 +0300

    fix staticfiles

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATCI_ROOT = os.path.join(BASE_DIR, 'statifiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR
-]
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # redirect on succesful login
 LOGIN_REDIRECT_URL = 'home'
